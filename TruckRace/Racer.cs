a4566cb883ee978da5c53b23cde5464bbc48e02a
ramancheema34143 <ramancheema34143@gmail.com>
2019-11-27 17:48:44 +1300

    Add project files.

---
+++ TruckRace/Racer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TruckRace
+{
+    public abstract class Racer
+    {
+        public string Name;
+        public Bet bet;
+        public int Cash;
+        public bool busted;
+        public Label statusLabel, MaximumBet;
+
+        public Racer(Bet bet, Label MaximumBet, int Cash, Label statusLabel)
+        {
+            this.bet = bet;
+            this.Cash = Cash;
+            this.MaximumBet = MaximumBet;
+            this.statusLabel = statusLabel;
+            if (this.statusLabel != null)
+                this.statusLabel.ForeColor = System.Drawing.Color.Black;
+
+        }
+
+        public void UpdateLabels()
+        {
+            if (bet == null)
+            {
+                statusLabel.Text = String.Format("{0} hasn't placed any bets", Name);
+            }
+
+            else
+            {
+                statusLabel.Text = bet.Descr();
+            }
+            if (Cash == 0)
+            {
+                busted = true;
+                statusLabel.Text = String.Format("BUSTED!");
+                statusLabel.ForeColor = System.Drawing.Color.Red;
+
+            }
+            MaximumBet.Text = String.Format("Maximum Bet: ${0}", Cash);
+        }
+
+
+        public void ClearBet()
+        {
+            bet.Amount = 0;
+        }
+
+        public bool PlaceBet(int Amount, int Trucks)
+        {
+            if (Amount <= Cash)
+            {
+                bet = new Bet(Amount, Trucks, this);
+                return true;
+            }
+            return false;
+        }
+
+        public void Collect(int Winner)
+        {
+            Cash += bet.Pay(Winner);
+        }
+
+        public abstract void setRacerName();
+    }
+
+
+    public class Rajdeep : Racer
+    {
+        public Rajdeep(Bet MyBet, Label MaximumBet, int Cash, Label MyLabel) : base(MyBet, MaximumBet, Cash, MyLabel)
+        {
+        }
+
+        public override void setRacerName()
+        {
+            Name = "Rajdeep";
+        }
+    }
+
+    public class Lovedeep : Racer
+    {
+        public Lovedeep(Bet bet, Label MaximumBet, int Cash, Label label) : base(bet, MaximumBet, Cash, label)
+        {
+        }
+
+        public override void setRacerName()
+        {
+            Name = "Lovedeep";
+        }
+    }
+
+    public class Raman : Racer
+    {
+        public Raman(Bet bet, Label MaximumBet, int Cash, Label label) : base(bet, MaximumBet, Cash, label)
+        {
+        }
+
+        public override void setRacerName()
+        {
+            Name = "Raman";
+        }
+    }
+
+    //coding is for  factory
+    public class Factory
+    {
+        public Racer getRacer(String Name, Label MaximumBet, Label bet)
+        {
+            Racer Obj_race;
+            switch (Name.ToLower())
+            {
+                case "rajdeep":
+                    Obj_race = new Rajdeep(null, MaximumBet, 50, bet);
+                    break;
+
+                case "raman":
+                    Obj_race = new Raman(null, MaximumBet, 50, bet);
+                    break;
+
+                case "lovedeep":
+                    Obj_race = new Lovedeep(null, MaximumBet, 50, bet);
+                    break;
+
+                default:
+                    Obj_race = null;
+                    break;
+            }
+            Obj_race.setRacerName();
+            return Obj_race;
+        }
+    }
+
+}
---
+++ TruckRaceUnitTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TruckRace;
+namespace TruckRaceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Factory Obj_Factory = new Factory();
+        Racer Lovedeep;
+        Trucks[] Truckss = new Trucks[1];
+
+        [TestMethod]
+        public void TestWinnerOutcome()
+        {
+            Trucks.StartingPosition1 = 0;
+            Trucks.RacetrackLength1 = 50;
+            int TruckRaceAmount = 50;
+            int TrucksNumber = 1;
+            int expectedWin = 100;
+            int expectedLose = 0;
+            Truckss[0] = new Trucks() { TrucksPictureBox = null };
+            //  Truckss[1] = new Trucks() { TrucksPictureBox = null };
+            Lovedeep = Obj_Factory.getRacer("Lovedeep", null, null);
+            Lovedeep.Cash = TruckRaceAmount;
+            Lovedeep.PlaceBet((int)TruckRaceAmount, TrucksNumber);
+
+            bool nowin = true;
+            int win = -1;
+            while (nowin)
+            {
+                for (int i = 0; i < Truckss.Length; i++)
+                {
+                    if (Trucks.Run(Truckss[i]))
+                    {
+                        win = i + 1;
+                        Lovedeep.Collect(win);
+                        nowin = false;
+
+                    }
+                }
+            }
+            if (Lovedeep.bet.TruckNum == win)
+            {
+                Assert.AreEqual(expectedWin, Lovedeep.Cash, "Account not credited correctly");
+            }
+            if (Lovedeep.bet.TruckNum != win)
+            {
+                Assert.AreEqual(expectedLose, Lovedeep.Cash, "Account not debited correctly");
+
+            }
+        }
+    }
+
+}
